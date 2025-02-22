185aac3132b9d71986b7c961cba19f0765fe0f9c
deepaksp24 <96986851+deepaksp24@users.noreply.github.com>
2025-02-22 22:46:28 +0530

    API for categories for home page (#1)

--- src/blueprints/__init__.py
+++ src/blueprints/__init__.py
@@ -1,4 +1,6 @@
 from src.blueprints.index import index_blueprint
+from src.blueprints.category import category_blueprint
 
 def init_blueprint(app):
     app.register_blueprint(index_blueprint,url_prefix='/')
+    app.register_blueprint(category_blueprint,url_prefix = '/category')
